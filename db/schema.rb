0b4718a61e39a33eb1b2c9d5133ba1ab85c8ef16
oleg.kmit <oleg.kmit@gmail.com>
2017-05-14 12:53:16 +0300

    Update migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -17,55 +16,53 @@ ActiveRecord::Schema.define(version: 20150406175914) do
   enable_extension "plpgsql"
 
   create_table "authentications", force: :cascade do |t|
-    t.integer  "user_id",    null: false
-    t.string   "provider",   null: false
-    t.string   "uid",        null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
   end
 
-  add_index "authentications", ["provider", "uid"], name: "index_authentications_on_provider_and_uid", using: :btree
-
   create_table "blocks", force: :cascade do |t|
-    t.string   "title",      null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id",    null: false
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "cards", force: :cascade do |t|
-    t.text     "original_text"
-    t.text     "translated_text"
-    t.datetime "review_date",                   null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id",                       null: false
-    t.string   "image"
-    t.integer  "block_id",                      null: false
-    t.integer  "interval",        default: 1,   null: false
-    t.integer  "repeat",          default: 1,   null: false
-    t.float    "efactor",         default: 2.5, null: false
-    t.integer  "attempt",         default: 1,   null: false
-    t.integer  "quality",         default: 5,   null: false
+    t.text "original_text"
+    t.text "translated_text"
+    t.datetime "review_date", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.string "image"
+    t.integer "block_id", null: false
+    t.integer "interval", default: 1, null: false
+    t.integer "repeat", default: 1, null: false
+    t.float "efactor", default: 2.5, null: false
+    t.integer "attempt", default: 1, null: false
+    t.integer "quality", default: 5, null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "crypted_password"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "salt"
-    t.string   "remember_me_token"
+    t.string "email"
+    t.string "crypted_password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "salt"
+    t.string "remember_me_token"
     t.datetime "remember_me_token_expires_at"
-    t.string   "reset_password_token"
+    t.string "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
-    t.integer  "current_block_id"
-    t.string   "locale"
+    t.integer "current_block_id"
+    t.string "locale"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
 
 end
